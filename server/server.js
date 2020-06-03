cfb2985e8c5a398dbf0a02a04b667829b41fac47
Kody Maus <auto.mausx@gmail.com>
2020-06-03 10:37:22 -0600

    basic file set up and express routes started

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const PORT = 9000;
+const queries = require('./queries');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/lift', (req, res) => {
+  res.send('get lift works!');
+});
+
+app.post('/lift', (req, res) => {
+  res.send('post a lift works!');
+});
+
+app.delete('/lift', (req, res) => {
+  res.send('delete a lift works!')
+});
+
+app.put('/lift', (req, res) => {
+  res.send('update lift works!')
+});
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require("webpack");
+const path = require('path');
+const SRC_DIR =  path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+
+module.exports = {
+  entry: [`${SRC_DIR}/index.js`],
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: SRC_DIR,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+};
